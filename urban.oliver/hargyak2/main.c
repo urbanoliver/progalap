f971bd730e696852fbc9d1c2863d8a8c8ff4a3ad
urbanoliver <90559472+urbanoliver@users.noreply.github.com>
2021-09-28 16:25:20 +0200

    Add files via upload

---
+++ urban.oliver/hargyak2/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int szam, i=2, db=0;
+    int talalt=0;
+
+    printf("Kerek egy szamot!\n");
+
+    scanf("%d", &szam);
+
+    /*for(i=2; i<=szam/2; i++)
+        if(szam % i == 0){
+            db +=1;} // db = db+1;
+        if(db){
+            printf("nem prim");}
+        else{
+            printf("prim");
+        }*/
+    /*while(i<=szam/2 && !talalt){
+            if(szam % 1){
+                i++
+                continue;
+
+
+            }else{
+                talalt=1;
+            }
+        }
+            if(talalt){
+                printf("nme prim");
+            }else{
+                printf("prim");
+
+            }*/
+
+
+
+
+
+
+
+    return 0;
+}
