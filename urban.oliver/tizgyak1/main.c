f6f5e654caaf60df4ffa976953fbb03e1083d67b
urbanoliver <90559472+urbanoliver@users.noreply.github.com>
2021-11-09 16:09:36 +0100

    Add files via upload

---
+++ urban.oliver/tizgyak1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct datum {
+    int ev;
+    int honap;
+    int nap;
+
+}
+void beolvas (Datum *d);
+void kiir (Datum d);
+Datum regebbi
+
+int main()
+{
+
+    return 0;
+}
